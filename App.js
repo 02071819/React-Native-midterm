5f66cccc32c140f46a1453489ab9b23be6e05f35
02071819 <54053565+02071819@users.noreply.github.com>
2021-07-05 23:52:21 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { colors } from "./styles";
+import HotelView from './screens/HotelView/index';
+
+export default function App() {
+  return (
+    <ScrollView style={styles.container}>
+      <HotelView />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.darkBg,
+  },
+});
